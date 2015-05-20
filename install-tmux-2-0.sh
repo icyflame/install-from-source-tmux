0ce30fc5d1a0802404abf55e2e5ada1b2321a4b7
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-20 12:51:30 +0530

    Add the main bash script.
    Untested, yet.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

---
+++ install-tmux-2-0.sh
@@ -0,0 +1,20 @@
+cd ~/sandbox
+tar zxvf ncurses-5.9.tar.gz
+tar zxvf libevent-2.0.22-stable.tar.gz
+tar zxvf tmux-2.0.tar.gz
+cd ncurses-5.9
+./configure
+make
+sudo make install
+cd ~/sandbox
+cd libevent-2.0.22-stable
+./configure
+make
+sudo make install
+cd ~/sandbox
+cd tmux-2.0
+./configure
+make
+sudo make install
+
+echo "Installed tmux 2.0! Type tmux, enter and start multiplexing!"
